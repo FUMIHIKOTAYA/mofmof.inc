3592fcb105fc463da2a556a031051dc3a7b13c32
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-02 08:57:13 +0900

    物件登録画面を作成

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,4 +1,30 @@
 class LeasesController < ApplicationController
+
   def index
   end
+
+  def new
+    @lease = Lease.new
+  end
+
+  def create
+    Lease.create(lease_params)
+    redirect_to new_lease_path
+  end
+
+  private
+
+  def lease_params
+    params.require(:lease).permit(
+      :lease_name,
+      :rent,
+      :address,
+      :age,
+      :note
+    )
+  end
+end
+
+
+
 end
