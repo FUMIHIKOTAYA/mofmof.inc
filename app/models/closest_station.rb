03b53e55c37168967b5bf6697aef109be32a4f84
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-01 10:24:23 +0900

    Lease・ClosestStationモデルでバリデーションを定義

--- app/models/closest_station.rb
+++ app/models/closest_station.rb
@@ -1,3 +1,7 @@
 class ClosestStation < ApplicationRecord
   belongs_to :lease
+
+  validates :route_name, presence: true, on: :update
+  validates :station_name, presence: true, on: :update
+  validates :minute_walk, presence: true, on: :update
 end
--- app/models/lease.rb
+++ app/models/lease.rb
@@ -1,4 +1,10 @@
 class Lease < ApplicationRecord
   has_many: closest_stations, dependent: :destroy
   accepts_nested_attributes_for :closest_stations, allow_destroy: true
+
+  validates :lease_name, presence: true, length: { maximum: 50 }
+  validates :rent, numericality: { only_integer: true }
+  validates :address, presence: true, length: { maximum: 100 }
+  validates :age, numericality: { only_integer: true }
+  validates :note, length: { maximum: 300 }, allow_blank: true
 end
