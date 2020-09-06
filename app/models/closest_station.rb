9bbbbff9f244e855585d2e233b6ae64b0ac5e14a
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-06 09:30:04 +0900

    条件付き（駅名は必須）バリデーションを追加

--- app/models/closest_station.rb
+++ app/models/closest_station.rb
@@ -1,7 +1,9 @@
 class ClosestStation < ApplicationRecord
   belongs_to :lease, inverse_of: :closest_stations
 
-  validates :route_name, presence: true, on: :update
-  validates :station_name, presence: true, on: :update
-  validates :minute_walk, presence: true, on: :update
+  validates :station_name, presence: true, if: :present_route_name_or_minute_walk?
+
+  def present_route_name_or_minute_walk?
+    route_name.present? || minute_walk.present?
+  end
 end
