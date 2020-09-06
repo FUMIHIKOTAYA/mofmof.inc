d96a3121311fc4a386c71462a32f543713e15f33
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-09-06 09:52:06 +0900

    入力数値を制限するバリデーションを追加

--- app/models/lease.rb
+++ app/models/lease.rb
@@ -3,8 +3,8 @@ class Lease < ApplicationRecord
   accepts_nested_attributes_for :closest_stations, allow_destroy: true, reject_if: :all_blank
 
   validates :lease_name, presence: true, length: { maximum: 50 }
-  validates :rent, numericality: { only_integer: true }
+  validates :rent, numericality: { only_integer: true, greater_than: 7000, less_than: 5310000 }
   validates :address, presence: true, length: { maximum: 100 }
-  validates :age, numericality: { only_integer: true }
+  validates :age, numericality: { only_integer: true, less_than: 300 }
   validates :note, length: { maximum: 300 }, allow_blank: true
 end
